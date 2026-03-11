304d77fdf788ee86444ccf7e11b644f2a3e893e2
scilla0515 <168850938+scilla0515@users.noreply.github.com>
2026-03-11 11:58:28 -0400

    Refactor nav.js to insert HTML at the beginning of body

---
+++ nav.js
@@ -0,0 +1,9 @@
+fetch('nav.html')
+  .then(res => res.text())
+  .then(html => {
+    document.body.insertAdjacentHTML('afterbegin', html);
+    const header = document.getElementById('main-header');
+    window.addEventListener('scroll', () => {
+      header.classList.toggle('scrolled', window.scrollY > 60);
+    });
+  });
